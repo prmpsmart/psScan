aeccb7e1db4776ab7f4a1dc4e7464a2b8d7739e7
Miracle Apata <prmpsmart@gmail.com>
2023-10-02 23:24:45 +0100

    used telebot

--- bot.py
+++ bot.py
@@ -1,28 +1,69 @@
-import os, socket
-import threading
-from telegram import ForceReply, Update
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-    MessageHandler,
-    filters,
-    Updater,
-)
-import requests
-import json
+import time as tm
+import telebot
+from telebot import types
+from crypto_coins import *
 
 TELEGRAM_BOT_TOKEN = "6522595578:AAEoohT3dDPq7JGczjSffttpl3NVR-GoY7Q"
 ETHERSCAN_APIKEY = "5VSCYDENMVJQUMZXP4JZP26Z1U8GVV3HN1"
 ETHEREUM_API_URL = "https://api.etherscan.io/api"
 
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN, threaded=False)
 
-# Ethereum:
-#    0x1d81D79d0D5cc899E16d8cdaD7995B8bEb6f7114 (Uniswap)
-#    0x4f0178AB2545c2c3629653B44588647f6f0059E3 (Chainlink)
-#    0x509faABc284aa9770437423b9f133863Da9cb728 (Tether)
 
-# Ethereum blockchain API URL
+def getMention(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    return "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+
+
+@bot.message_handler(commands=["start"])
+def command_start(message: types.Message):
+    print(type(message))
+
+    start_markup = types.ReplyKeyboardMarkup(
+        resize_keyboard=True,
+        one_time_keyboard=False,
+    )
+    start_markup.row("/start", "/help")
+    start_markup.row("/crypto", "/bal")
+    start_markup.row("/hide")
+    bot.send_message(
+        message.chat.id, "🤖 The bot has started!\n⚙ Enter /help to see bot's function's"
+    )
+    bot.send_message(
+        message.chat.id,
+        f"""Hi {getMention(message)}!\nThis bot is just a proof-of-work done by:\nMiracle Apata a.k.a @prmpsmart\n\nTo check the balance of a token address type /bal followed by the address\ne.g\n/bal 0x1d81D79d0D5cc899E16d8cdaD7995B8bEb6f7114""",
+        parse_mode="Markdown",
+    )
+    bot.send_message(
+        message.from_user.id,
+        "⌨️ The Keyboard is added!\n⌨️ /hide To remove kb",
+        reply_markup=start_markup,
+    )
+
+
+@bot.message_handler(commands=["hide"])
+def command_hide(message: types.Message):
+    hide_markup = types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, "⌨💤...", reply_markup=hide_markup)
+
+
+@bot.message_handler(commands=["help"])
+def command_help(message: types.Message):
+    bot.send_message(
+        message.chat.id,
+        "🤖 /start - display the keyboard\n"
+        "💎 /crypto - current cryptocurrency\n"
+        "⌛️ /bal - calculate eth aadress bal\n",
+    )
+
+
+@bot.message_handler(commands=["crypto"])
+def command_crypto(message: types.Message):
+    coins_markup = types.InlineKeyboardMarkup(row_width=1)
+    for key, value in coins.items():
+        coins_markup.add(types.InlineKeyboardButton(text=key, callback_data=value))
+    bot.send_message(message.chat.id, "📃 Choose the coin:", reply_markup=coins_markup)
 
 
 # Function to scan the token contract on the Ethereum blockchain
@@ -37,132 +78,47 @@ def address_balance(token_contract_address):
     }
 
     response = requests.get(ETHEREUM_API_URL, params=params)
-    contract_information = json.loads(response.content.decode())
+    contract_information = response.json()
 
     return contract_information
 
 
-# Function to scan the token contract on the Ethereum blockchain
-def scan_token_contract(token_contract_address):
-    """Scans the token contract on the Ethereum blockchain and returns the contract information."""
-
-    params = {
-        "module": "contract",
-        "action": "getabi",
-        "address": token_contract_address,
-        "apikey": ETHERSCAN_APIKEY,
-    }
-
-    response = requests.get(ETHEREUM_API_URL, params=params)
-    contract_information = json.loads(response.content.decode())
-
-    return contract_information
-
-
-# Function to calculate the amount of tax received in the marketing wallet
-def calculate_tax_received_in_marketing_wallet(contract_information):
-    """Calculates the amount of tax received in the marketing wallet."""
-
-    # Get the marketing wallet address from the contract information
-    marketing_wallet_address = contract_information["result"]["marketingWallet"]
-
-    # Calculate the amount of tax received in the marketing wallet
-    tax_received_in_marketing_wallet = 0
-
-    # TODO: Implement the logic to calculate the amount of tax received in the marketing wallet
-
-    return tax_received_in_marketing_wallet
-
-
-# Function to send a report to the user
-def send_report_to_user(bot, user_id, tax_received_in_marketing_wallet):
-    """Sends a report to the user with the amount of tax received in the marketing wallet."""
-
-    bot.sendMessage(
-        user_id,
-        f"The amount of tax received in the marketing wallet is {tax_received_in_marketing_wallet}.",
-    )
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context.
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
-    text = update.message.text.strip()
-    user = update.effective_user
-
-    if text.startswith("/") and text[1:].lower().split()[0] not in [
-        "",
-        "start",
-        "help",
-        "bal",
-    ]:
-        await update.message.reply_html(
-            rf"""Hi {user.mention_html()}!
-
-Supported commands are: /start, /help, /bal"""
-        )
-    else:
-        await update.message.reply_html(
-            rf"""Hi {user.mention_html()}!
-This bot is just a proof-of-work done by:
-Miracle Apata a.k.a @prmpsmart
-
-To check the balance of a token address type /bal followed by the address
-e.g
-
-/bal 0x1d81D79d0D5cc899E16d8cdaD7995B8bEb6f7114""",
-            # reply_markup=ForceReply(selective=True),
-        )
-
-
-async def bal(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Checks the balance of the address given by the user"""
-    text = update.message.text.strip()
+@bot.message_handler(commands=["bal"])
+def command_bal(message: types.Message):
+    text = message.text.strip()
 
     if text:
         text_ = text.split()[-1]
         contract_information = address_balance(text_)
-        text = """STATUS: {status}
-MESSAGE: {message}
-RESULT: {result}
-
-
-USER_INPUT: {text}""".format(
+        text = """STATUS: {status}\nMESSAGE: {message}\nRESULT: {result}\nUSER_INPUT: {text}""".format(
             text=text, **contract_information
         )
-        await update.message.reply_text(text)
+        bot.reply_to(message, text)
 
     else:
-        await update.message.reply_text("Enter an address!")
+        bot.reply_to(message, "Enter a valid address!")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await start(update, context)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_crypto_stocks(call):
+    if call.message:
+        coins_switcher = {
+            "BTC": f"💰Bitcoin:  ${btc_price}",
+            "LTC": f"💰Litecoin:  ${ltc_price}",
+            "ETH": f"💰Ethereum:  ${eth_price}",
+            "ETC": f"💰Ethereum Classic:  ${etc_price}",
+            "ZEC": f"💰Zcash:  ${zec_price}",
+            "DSH": f"💰Dash:  ${dsh_price}",
+            "XRP": f"💰Ripple:  ${xrp_price}",
+        }
+
+        coin_response = coins_switcher.get(call.data)
+        if coin_response:
+            bot.send_message(call.message.chat.id, coin_response)
 
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
-    # updater = Updater(token=TELEGRAM_BOT_TOKEN, use_context=True)
-    # application = updater.dispatcher
-
-    # on different commands - answer in Telegram
-    application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("bal", bal))
-
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.COMMAND, start))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
-    # application.run_webhook(listen="0.0.0.0", port=8000)
-
-
-if __name__ == "__main__":
-    p = socket.socket()
-    p.bind(("0.0.0.0", 80))
-    threading.Thread(target=p.listen, args=[5]).start()
-    main()
+while True:
+    try:
+        bot.infinity_polling()
+    except Exception:
+        tm.sleep(1)
